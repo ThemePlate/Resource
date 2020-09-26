871cb5fb3f0a5dca7c42c85a970a327caf52ab80
kermage <genealyson.torcende@gmail.com>
2020-09-26 14:20:15 +0800

    basic dns prefetching

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ThemePlate Resource
+
+## Usage
+
+```php
+add_action( 'wp_head', array( 'ThemePlate\Preload', 'init' ), 2 );
+
+ThemePlate\Resource::hint( '//cdnjs.cloudflare.com' );
+
+```
--- Resource.php
+++ Resource.php
@@ -10,4 +10,23 @@
 namespace ThemePlate;
 
 class Resource {
+
+	private static $storage = array();
+
+
+	public static function hint( $resource ) {
+
+		self::$storage[] = $resource;
+
+	}
+
+
+	public static function init() {
+
+		foreach ( self::$storage as $resource ) {
+			echo "<link rel='dns-prefetch' href='{$resource}' />\n";
+		}
+
+	}
+
 }
